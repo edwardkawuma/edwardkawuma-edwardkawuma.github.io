85fc0744e74639234dd2e019a76770644418b39e
edward kawuma <eddykawuma@gmail.com>
2026-08-04 00:07:37 +0300

    Improve UI/UX and add GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/edwardkawuma-edwardkawuma.github.io/',
   plugins: [react()],
   resolve: {
     alias: {
