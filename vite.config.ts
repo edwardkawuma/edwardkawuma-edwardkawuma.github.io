6e76be3aa097d0c99adcb47af5c94170e24c365a
edward kawuma <eddykawuma@gmail.com>
2026-08-04 00:11:44 +0300

    Revert "Improve UI/UX and add GitHub Pages deployment workflow"
    This reverts commit 85fc0744e74639234dd2e019a76770644418b39e.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/edwardkawuma-edwardkawuma.github.io/',
   plugins: [react()],
   resolve: {
     alias: {
